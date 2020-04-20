3fcd09a5f63fececf8057c4273a76dfd80102c8c
ViniciusLNascimento <vin.nascimento@outlook.com>
2020-04-20 17:57:35 -0300

    Subindo arquivos de floresta

---
+++ Assets/Scripts/OpenMap/CameraTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject myCamera;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CamerasController.instance.EnableCamera(myCamera);
+        }
+    }
+}
