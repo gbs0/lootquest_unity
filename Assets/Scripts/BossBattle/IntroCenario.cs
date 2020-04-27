2deca1767f4567820d2974c22ed1f564a535fc10
gb_s0 <gabrielschiavo0@gmail.com>
2020-04-27 17:05:22 -0300

    Adicionando efeito de ataque boss

--- Assets/Scripts/BossBattle/IntroCenario.cs
+++ Assets/Scripts/BossBattle/IntroCenario.cs
@@ -7,10 +7,11 @@ public class IntroCenario : MonoBehaviour
      
     SpriteRenderer m_SpriteRenderer;
     Color m_NewColor;  // Retorno da cor em float m_r, m_g, m_b
-    public ParticleSystem particulaFogo;
+
     Color corFinal = new Color(0f ,0f, 0f, 0f);
     private GameObject[] arvores;
-
+    private GameObject[] particulasFogo;
+    private ParticleSystem particula;
 
     void Awake()
     {  
@@ -27,22 +28,31 @@ public class IntroCenario : MonoBehaviour
         {
            arvores = GameObject.FindGameObjectsWithTag("Trees");      
         } 
+
+        if (particulasFogo == null)
+        {
+            particulasFogo = GameObject.FindGameObjectsWithTag("FireParticles");
+        }
 	    
         if (arvores.Length == 0)
 	    {
            Debug.Log("Nenhuma Arvore no Array[]");
         }
 
-        foreach (GameObject arvore in arvores) // Mudar a cor das arvores && Iniciar particula de fogo
+        foreach (GameObject arvore in arvores) 
         {
-	        // m_SpriteRenderer.color = Color.blue;
+            // m_SpriteRenderer.color = Color.blue;
             // spriteColor = Color.blue;
             // spriteColor = corFinal;
-            // Quaternion rotationParticula = new Quaternion(arvore.transform.rotation.x, arvore.transform.rotation.y, arvore.transform.rotation.z, 0f );
-            Quaternion rotationParticula = new Quaternion( -90f, arvore.transform.rotation.y, arvore.transform.rotation.z, 0f );
-            // Instantiate(particulaFogo, arvore.transform.position, arvore.transform.rotation);
-            Instantiate(particulaFogo, arvore.transform.position, rotationParticula);
         }
+
+        foreach (GameObject particulaFogo in particulasFogo) // Mudar a cor das arvores && Iniciar particula de fogo
+        {  
+            particula = particulaFogo.GetComponent<ParticleSystem>();
+            particula.Emit(1);
+        }
+
+
     }
 
     // Update is called once per frame
