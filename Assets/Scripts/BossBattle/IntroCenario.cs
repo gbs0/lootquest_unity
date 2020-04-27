58a300aa8963a80bb85d56fc5cfaa464d1a33693
gb_s0 <gabrielschiavo0@gmail.com>
2020-04-27 14:09:56 -0300

    Adicionando tags aos GameObjects do cenario

--- Assets/Scripts/BossBattle/IntroCenario.cs
+++ Assets/Scripts/BossBattle/IntroCenario.cs
@@ -7,7 +7,7 @@ public class IntroCenario : MonoBehaviour
      
     SpriteRenderer m_SpriteRenderer;
     Color m_NewColor;  // Retorno da cor em float m_r, m_g, m_b
-    public ParticleSystem fogo;
+    public ParticleSystem particulaFogo;
     Color corFinal = new Color(0f ,0f, 0f, 0f);
     private GameObject[] arvores;
 
@@ -24,13 +24,24 @@ public class IntroCenario : MonoBehaviour
     void Start()
     {
         if (arvores == null)
-          arvores = GameObject.FindGameObjectsWithTag("Trees");
+        {
+           arvores = GameObject.FindGameObjectsWithTag("Trees");      
+        } 
+	    
+        if (arvores.Length == 0)
+	    {
+           Debug.Log("Nenhuma Arvore no Array[]");
+        }
 
         foreach (GameObject arvore in arvores) // Mudar a cor das arvores && Iniciar particula de fogo
         {
-	// m_SpriteRenderer.color = Color.blue;
-        // spriteColor = Color.blue;
-        // spriteColor = corFinal;
+	        // m_SpriteRenderer.color = Color.blue;
+            // spriteColor = Color.blue;
+            // spriteColor = corFinal;
+            // Quaternion rotationParticula = new Quaternion(arvore.transform.rotation.x, arvore.transform.rotation.y, arvore.transform.rotation.z, 0f );
+            Quaternion rotationParticula = new Quaternion( -90f, arvore.transform.rotation.y, arvore.transform.rotation.z, 0f );
+            // Instantiate(particulaFogo, arvore.transform.position, arvore.transform.rotation);
+            Instantiate(particulaFogo, arvore.transform.position, rotationParticula);
         }
     }
 
