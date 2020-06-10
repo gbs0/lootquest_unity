53abea0b6e5f8178c4cd337fa938b3dc136c72a1
gabriel schiavo <gabriel@lewagon.org>
2020-06-10 18:26:10 -0300

    adicionando sprite na hud de xp e script de xp

---
+++ Assets/ControleXP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleXP : MonoBehaviour
+{
+    int m_XP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_XP = PlayerPrefs.GetInt("_xp", 0);
+        SetXP();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetXP()
+    {
+        while(m_XP < 200)
+        {
+            return;
+        }
+
+        
+    }
+}
