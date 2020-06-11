950f2e4da051b55edab0bec1d31ad1e59353367e
Matheus <matheus.mello@vetorzero.com.br>
2020-06-10 22:32:28 -0300

    arrumando menu de pause

---
+++ Assets/Scripts/MenuChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuChecker : MonoBehaviour
+{
+    public GameObject menuprefab;
+
+    public OptionsMenu menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = FindObjectOfType<OptionsMenu>();
+        if (menu==null)
+        {
+            Instantiate(menuprefab);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
