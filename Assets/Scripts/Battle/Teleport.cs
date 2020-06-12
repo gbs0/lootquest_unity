66a0955a4068b28e90c2a1ef675786fd2e63728d
ViniciusLNascimento <vin.nascimento@outlook.com>
2020-06-12 00:35:04 -0300

    BruxaTele

--- Assets/Scripts/Battle/Teleport.cs
+++ Assets/Scripts/Battle/Teleport.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
+    public GameObject Cait;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,7 @@ public class Teleport : MonoBehaviour
     // Update is called once per frame
     public void Telepot()
     {
-      
+        CaitNPC.tel = true;
     }
 
 }
